c66a84f916540effdf83a518348cfdd15d8fc922
jm61 <jean.moirano@gmail.com>
2022-08-23 06:15:30 +0800

    reversed svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -29,12 +29,7 @@ const config = {
 		prerender: {
 			default: true
 		},
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		})
+		adapter: adapter()
 	}
 };
 
