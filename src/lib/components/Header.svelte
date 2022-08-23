a3fc3a2937155a25d6de9302505ef327b5578387
jm61 <jean.moirano@gmail.com>
2022-08-23 09:36:01 +0800

    added square anim and some css fix

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -13,27 +13,42 @@
 		Skip to main content
 	</a>
 
-	<a href="/" class="site-title">
-		<img src="/images/logo.webp" alt="logo" class="logo" />
-	</a>
+	<div class="contLogo">
+		<a href="/" class="site-title" title="Home">
+			<img src="/images/logo.webp" alt="logo" class="logo" />
+		</a>
+	</div>
+
+	<div class="box" />
 
 	<HamburgerMenuButton />
 	<MainNav />
 </header>
 
 <style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+		border-bottom: 3px solid var(--accent);
+		background: rgb(130, 138, 109);
+		background: linear-gradient(90deg, rgb(149, 170, 95) 38%, rgba(169, 132, 103, 1) 73%);
+	}
+	.contLogo {
+		max-width: 50%;
+	}
 	.logo {
-		display: block;
-		width: 20%;
+		width: 40%;
 		border-radius: 10%;
 		box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.4);
 	}
 	.logo:hover {
-		box-shadow: 10px 10px 20px rgba(1000, 0, 0, 0.4);
+		box-shadow: 10px 10px 20px rgba(50, 100, 0, 0.4);
 	}
 	@media screen and (max-width: 380px) {
 		.logo {
-			width: 40%;
+			width: 50%;
 		}
 	}
 </style>
